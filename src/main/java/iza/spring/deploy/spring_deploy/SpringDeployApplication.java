f8eddb86907a83d2e1e44f074801dd1eb43f6d28
William Fontelas <williamdevsuporte@gmail.com>
2024-08-20 13:54:07 -0300

    projeto de estudo de deploy com spring construido no initialzr

---
+++ src/main/java/iza/spring/deploy/spring_deploy/SpringDeployApplication.java
@@ -0,0 +1,13 @@
+package iza.spring.deploy.spring_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iza/spring/deploy/spring_deploy/SpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package iza.spring.deploy.spring_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
